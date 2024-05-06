fe69c51c810b3171e4ad888a62b886433d87c66b
deepforce123 <deepforce06@gmail.com>
2024-05-06 21:56:58 +0100

    fix

--- main.py
+++ main.py
@@ -313,7 +313,7 @@ class announce_embed(ui.Modal, title = 'Announcement embed'):
         
         if self.server_notice:
             embed = discord.Embed(title=self.heading.value, description=desc, url="https://duobot.com/p/deepforce", colour=maincolour, timestamp=datetime.datetime.now())
-            embed.set_footer(text = "Visit our website at https://duobot.com/p/deepforce", icon_url=interaction.user.avatar.url)
+            embed.set_footer(text = "Visit our website at https://duobot.com", icon_url=interaction.user.avatar.url)
         else:
             embed = discord.Embed(title=self.heading.value, description=desc, colour=maincolour)
             # embed.set_footer(text = "Visit our website at https://duobot.com")
