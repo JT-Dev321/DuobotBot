3df50e387194f99a4a10cd3131ea0d4ec85cbbb6
deepforce123 <deepforce06@gmail.com>
2024-05-11 20:49:03 +0100

    fix idk

--- main.py
+++ main.py
@@ -428,11 +428,14 @@ async def on_message(message : discord.Message):
                         found = True
                 if not found:
                     f.write(f"{message.author.id},")
-                    await message.reply(f"""## Looks like it's your first time here {message.author.mention}, welcome!
-                                        
-                                        Be sure to check out {faqchannelmention} and {autosupchannelmention} for immediate support.
-                                        Please feel free to open a ticket in {ticketchannelmention} with any further questions you have!""",
-                                        delete_after=120)
+                    await message.reply(
+                        f"""
+                        ## Looks like it's your first time here {message.author.mention}, welcome!
+                                            
+                        Be sure to check out {faqchannelmention} and {autosupchannelmention} for immediate support.
+                        Please feel free to open a ticket in {ticketchannelmention} with any further questions you have!
+                        """,
+                        delete_after=120)
 
 def insert_returns(body):
     if isinstance(body[-1], ast.Expr):
